6e20a7e614ef4086bba674149bb097a0756732c7
Stephan Dreyer <s.dreyer@skytron-energy.com>
2018-09-10 11:29:58 +0200

    Extended test with assertions

--- src/test/java/com.github.crunc.springboot.restdocs/GreetingDocumentation.java
+++ src/test/java/com.github.crunc.springboot.restdocs/GreetingDocumentation.java
@@ -1,20 +1,18 @@
 package com.github.crunc.springboot.restdocs;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.RestDocumentation;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebIntegrationTest(randomPort = true)
@@ -29,41 +27,18 @@ public class GreetingDocumentation {
     @Value("${local.server.port}")
     private int port;
 
-    @Autowired
-    private WebApplicationContext ctx;
-
-    private MockMvc mvc;
-
-//    @Before
-//    public void setup() {
-//        mvc = MockMvcBuilders.webAppContextSetup(ctx)
-//                .apply(MockMvcRestDocumentation.documentationConfiguration(docs))
-//                .build();
-//    }
-
     @Test
     public void sample() throws Exception {
-
-
-        docs.document("getGreetings",
-                responseFields(
-                        fieldWithPath("[].message").description("The greeting message")
-                )
-        ).getForEntity("http://localhost:" + port + "/greetings", Greeting[].class);
 
         final ResponseEntity<Greeting[]> res = docs.document("getGreetings")
                 .responseField(fieldWithPath("[].message").description("The greeting message"))
                 .get("http://localhost:" + port + "/greetings")
                 .forEntity(Greeting[].class);
 
-//        mvc.perform(get("/greetings"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
-//                .andDo(document("getGreetings",
-//                        responseFields(
-//                                fieldWithPath("[].message").description("The greeting message")
-//                        )
-//                ));
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getContentType()).isEqualTo(MediaType.valueOf("application/json;charset=UTF-8"));
+        assertThat(res.getBody()).containsExactly(new Greeting("Hello REST Docs"));
+
     }
 
 
